66e51475de901d99327a57aca153ef189c186e7c
badrinathvm@gmail.com <badrinathvm@gmail.com>
2019-05-10 08:39:54 -0700

    Initial push

---
+++ StringExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  StringExtensions.swift
+//  BadriRepo
+//
+//  Created by Badrinath on 5/10/19.
+//
+
+import Foundation
+
+
+extension String {
+    
+    public func isPrivatePod() {
+        print("Pod Testing successful")
+    }
+}
